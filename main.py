74415181b2c5207f7a1516bf88a8ff6c911fc7b1
Jack <71041042+Haxor1611@users.noreply.github.com>
2022-07-14 10:41:52 +0100

    added more functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from asyncore import write
 import tkinter as tk
 import json
 loggedIn = False
@@ -24,6 +25,7 @@ def login(users, username, password):
             break
         else:
             print("failed")
+
 def hashpassword(password):
         print("hashing")
         
@@ -32,6 +34,10 @@ class User:
         self.username = username
         self.password = password
 
+class Students:
+    def __init__(self, students):
+        self.students = students
+
 class Student:
     def __init__(self, fname, lname, paper1, paper2, paper3):
         self.fname = fname
@@ -39,9 +45,14 @@ class Student:
         self.paper1 = paper1
         self.paper2 = paper2
         self.paper3 = paper3
+        
     def calcpercent(self):
         total = self.paper1 + self.paper2 + self.paper3
         percentage = int((total/240) * 100)
+        if percentage > 90:
+            self.grade = 9
+        elif percentage > 80:
+            self.grade = 8
         return percentage
         
 #users = loadJson("users.json")
@@ -53,24 +64,31 @@ def inputScores(name, lname, paper1, paper2, paper3):
     if loggedIn ==True:
         print("Name: " , name, lname, "\n", "Total for score: " , (paper1 + paper2 + paper3))
         student = Student(name, lname, paper1, paper2, paper3)
-        students.append(student)
-        tk.messagebox.showinfo("Authentication error", "You  must be logged in!")
+        print(str(student.calcpercent()) + "%")
+        students.append(student.__dict__)
+        
+        tk.messagebox.showinfo("Authentication error", "Submitted scores for: " + name)
         
     else:
         tk.messagebox.showinfo("Authentication error", "You  must be logged in!")
         
 
-    
+def completedScores():
+    print(students)
+    studentObj = Students(students)
+    print(studentObj.__dict__)
+    writeJson(studentObj.__dict__, "students.json")
+
 def draw():
     master = tk.Tk()
     master.title("Test Results system")
     master.geometry("500x1000")
     tk.Label(master, text="Class marking system", padx = 10, font=("Arial", 25)).grid(row=0, column = 4)
-    tk.Label(master, text="First Name").grid(row=1, column=3)
-    tk.Label(master, text="Last Name").grid(row=2, column=3)
-    tk.Label(master, text="Paper 1: ").grid(row=3, column=3)
-    tk.Label(master, text="Paper 2: ").grid(row=4, column=3)
-    tk.Label(master, text="Paper 3: ").grid(row=5, column=3)
+    tk.Label(master, text="First Name").grid(row=1, column=3,pady=2)
+    tk.Label(master, text="Last Name").grid(row=2, column=3,pady=2)
+    tk.Label(master, text="Paper 1: ").grid(row=3, column=3,pady=2)
+    tk.Label(master, text="Paper 2: ").grid(row=4, column=3,pady=2)
+    tk.Label(master, text="Paper 3: ").grid(row=5, column=3,pady=2)
     
     e1 = tk.Entry(master)
     e2 = tk.Entry(master)
@@ -78,26 +96,26 @@ def draw():
     e4 = tk.Entry(master)
     e5 = tk.Entry(master)
 
-    e1.grid(row=1, column=4)
-    e2.grid(row=2, column=4)
-    e3.grid(row=3, column=4)
-    e4.grid(row=4, column=4)
-    e5.grid(row=5, column=4)
+    e1.grid(row=1, column=4,pady=2)
+    e2.grid(row=2, column=4,pady=2)
+    e3.grid(row=3, column=4,pady=2)
+    e4.grid(row=4, column=4,pady=2)
+    e5.grid(row=5, column=4,pady=2)
 
     button = tk.Button(master, text="Submit scores", width=30,pady=5, command=lambda: inputScores(str(e1.get()), str(e2.get()), int(e3.get()), int(e4.get()), int(e5.get()))).grid(row=9, column=4)
+    completeButton = tk.Button(master, text= "Completed Adding scores", width=19, padx=20, command=completedScores).grid(row=10, column=4,pady=15)
 
 
 
-
-    tk.Label(master, text="Login", font=("Arial", 25)).grid(row=10, column = 4)
-    tk.Label(master, text="Username").grid(row=11, column=3)
-    tk.Label(master, text="Password").grid(row=12, column=3)
+    tk.Label(master, text="Login", font=("Arial", 25)).grid(row=11, column = 4)
+    tk.Label(master, text="Username").grid(row=12, column=3,pady=2)
+    tk.Label(master, text="Password").grid(row=13, column=3,pady=2)
     usernameinput = tk.Entry(master)
     passwordinput = tk.Entry(master)
 
-    usernameinput.grid(row=11, column=4)
-    passwordinput.grid(row=12, column=4)
+    usernameinput.grid(row=12, column=4,pady=2)
+    passwordinput.grid(row=13, column=4,pady=2)
 
-    loginButton = tk.Button(master, text="Login", width = 30,pady=5, command = lambda: login(users, str(usernameinput.get()), str(passwordinput.get()))).grid(row=14, column=4)
+    loginButton = tk.Button(master, text="Login", width = 30,pady=5, command = lambda: login(users, str(usernameinput.get()), str(passwordinput.get()))).grid(row=15, column=4,pady=2)
     
 draw()
